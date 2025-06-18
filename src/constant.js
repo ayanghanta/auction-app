00365c5ec4dbed2a4b4aa337777be3a3179521c6
ayanghanta <ayanghanta674@gmail.com>
2025-06-18 10:19:02 +0530

    fix the url

--- src/constant.js
+++ src/constant.js
@@ -1,11 +1,16 @@
-// export const BASE_URL = `http://localhost:3000`;
-export const BASE_URL = `https://vintage-vault-server.onrender.com`;
+export const BASE_URL = `http://localhost:3000`;
+// export const BASE_URL = `https://vintage-vault-server.onrender.com`;
 export const RES_PER_PAGE_HOME = 6;
 
+const RESOURSE_URL = "https://pub-d6a7e6da52644dd6a6b521489ffdcba2.r2.dev";
+
 // PUBLIC URLS
-export const PRODUCT_IMG_URL = `${BASE_URL}/images/products`;
-export const USER_IMG_URL = `${BASE_URL}/images/users`;
-export const DOC_URL = `${BASE_URL}/documents`;
+// export const PRODUCT_IMG_URL = `${BASE_URL}/images/products`;
+// export const USER_IMG_URL = `${BASE_URL}/images/users`;
+// export const DOC_URL = `${BASE_URL}/documents`;
+export const PRODUCT_IMG_URL = `${RESOURSE_URL}`;
+export const USER_IMG_URL = `${RESOURSE_URL}`;
+export const DOC_URL = `${RESOURSE_URL}`;
 
 // MINIMUM BID HICK ON PREVIOUS BID
 export const HIKE = 10;
